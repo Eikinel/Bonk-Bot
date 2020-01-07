38dfc1b7bb2ed6560232676820575e52364ce6f4
Adrien <adrien.keller@epitech.eu>
2020-01-07 18:13:21 +0100

    fix on last modif of chan.lua

--- commands/chan.lua
+++ commands/chan.lua
@@ -2,13 +2,13 @@ local embed = require('tools/embed')
 local conf = require('conf')
 
 registerCommand({"chan"}, function(msg)
-    msg:delete()
     membed = embed:new()
-
+    
     membed:setColor(conf.colorChart.default)
     membed:setAuthor("State of command :", "", msg.client.user:getAvatarURL())
     membed:setDescription("Le resultat de cette commande est... " .. msg.client.owner.mentionString)
     membed:addField(
-    "Message deleted with a lot of successful success !!"
-    Message.channel:send({embed = membed})
+        "Message deleted with a lot of successful success !!")
+        Message.channel:send({embed = membed})
+    msg:delete()
 end)
