59b9398fb6afbf4bbfdc65abf67fd35e542a2352
Adrien KELLER <adrien.keller@epitech.eu>
2020-01-09 19:29:53 +0100

    add ' '-'channel delete <chan_id>' function

--- commands/chan.lua
+++ commands/chan.lua
@@ -1,12 +1,18 @@
-registerCommand({"chan"}, function(msg)
-    local category = msg.channel.category
-    local newChan = msg.guild:createTextChannel("channel_created")
+local log = require('../tools/log')
 
-    if (newChan) then
+registerCommand({"chan", "channel"}, function(msg, args)
+    local category = msg.channel.category
+    local guild = msg.guild
+    
+    if (args[1] == "create" and args[2]) then
+        local newChan = msg.guild:createTextChannel(args[2])
+        newChan:setCategory(category)
         msg.channel:send("Channel created")
-    else
-        msg.channel:send("Error while creating channel")
+    elseif (args[1] == "delete" and args[2]) then
+        local chan = guild:getChannel(args[2])
+        msg.channel:send("Deleted channel \"".. chan.name .. "")
+        chan:delete()
     end
-    newChan:setCategory(category)
+    --remove next line after debug
     msg:delete()
 end)
