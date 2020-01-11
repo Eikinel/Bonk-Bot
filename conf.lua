561d55a223f87be0eabdfbe79658bcbb273be906
Eikinel <valentin.pierrel@epitech.eu>
2020-01-11 17:52:00 +0100

    wip: Redo

--- conf.lua
+++ conf.lua
@@ -2,10 +2,6 @@
 local conf = {}
 conf.__index = conf
 
-conf.colorChart = {
-    default = 0xF02E89
-}
-
 conf.folders = {
     commands = "commands/",
     tools = "tools/"
@@ -14,6 +10,13 @@ conf.folders = {
 conf.guild = {
     id = "663798033669947423",
     channels = { "663801770933354506" }
+}
+
+conf.bot = {
+    prefix = "'-'",
+    colors = {
+        default = 0xC8C846
+    }
 }
 
 return conf
